01054046876ef2434bd180b3463d993eb7e08c2b
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2015-08-20 13:34:52 +0200

    - and / are now fixed

--- test/tree-ops-test.js
+++ test/tree-ops-test.js
@@ -32,76 +32,93 @@ var treeOps = require( '../lib/tree-ops.js' ) ;
 
 
 
-describe( "Basic tests" , function() {
+describe( "Operator behaviours" , function() {
 	
-	it( "+ and * mixing and order" , function() {
+	it( "mixing + and * for the same base key should preserve operation order (first *, then +)" , function() {
 		
 		var creature = {
-			hp: 8,
-			attack: 5,
-			defense: 3,
+			hp: 8 ,
+			attack: 5 ,
+			defense: 3 ,
 			move: 1
 		} ;
 		
 		var shield = {
-			"+defense": 3,
+			"+defense": 3 ,
 		} ;
 		
 		var enchantedArmor = {
-			"*defense": 2,
-			"+defense": 1,
+			"*defense": 2 ,
+			"+defense": 1 ,
 			"+magic": 1
 		} ;
 		
 		expect( treeOps.stack( creature , shield , enchantedArmor ) ).to.eql( {
-			hp: 8,
-			attack: 5,
-			defense: 3,
-			move: 1,
-			"+defense": 4,
-			"*defense": 2,
+			hp: 8 ,
+			attack: 5 ,
+			defense: 3 ,
+			move: 1 ,
+			"+defense": 4 ,
+			"*defense": 2 ,
 			"+magic": 1
 		} ) ;
 		
 		expect( treeOps.reduce( creature , shield , enchantedArmor ) ).to.eql( {
-			hp: 8,
-			attack: 5,
-			defense: 10,
-			move: 1,
+			hp: 8 ,
+			attack: 5 ,
+			defense: 10 ,
+			move: 1 ,
 			"+magic": 1
 		} ) ;
 		
 	} ) ;
 	
-	it( "- and / converted to + and *" , function() {
+	it( "- and / should be converted to + and *" , function() {
 		
 		var creature = {
-			hp: 8,
-			attack: 5,
-			defense: 3,
+			hp: 8 ,
+			attack: 5 ,
+			defense: 8 ,
 			move: 1
 		} ;
 		
 		var cursedAmulet = {
-			"-defense": 2,
+			"-defense": 2 ,
 		} ;
 		
 		var cursedRing = {
-			"/defense": 1,
+			"/defense": 2 ,
 		} ;
 		
 		expect( treeOps.stack( cursedAmulet ) ).to.eql( {
-			"+defense": -2,
+			"+defense": -2 ,
 		} ) ;
-		/*
-		expect( treeOps.reduce( creature , shield , enchantedArmor ) ).to.eql( {
-			hp: 8,
-			attack: 5,
-			defense: 10,
-			move: 1,
-			"+magic": 1
+		
+		expect( treeOps.stack( cursedRing ) ).to.eql( {
+			"*defense": 0.5 ,
 		} ) ;
-		*/
+		
+		expect( treeOps.stack( cursedAmulet , cursedRing ) ).to.eql( {
+			"+defense": -2 ,
+			"*defense": 0.5
+		} ) ;
+		
+		expect( treeOps.stack( creature , cursedAmulet , cursedRing ) ).to.eql( {
+			hp: 8 ,
+			attack: 5 ,
+			defense: 8 ,
+			"+defense": -2 ,
+			"*defense": 0.5 ,
+			move: 1
+		} ) ;
+		
+		expect( treeOps.reduce( creature , cursedAmulet , cursedRing ) ).to.eql( {
+			hp: 8 ,
+			attack: 5 ,
+			defense: 2 ,
+			move: 1
+		} ) ;
+		
 	} ) ;
 } ) ;
 
